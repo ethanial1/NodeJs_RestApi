67921c28c1edee80755526f2730c369102a8178c
ethanial1 <camiloeiz00@gmail.com>
2021-08-19 16:59:48 -0500

    Uso de la función

--- src/lib/bodyParser.js
+++ src/lib/bodyParser.js
@@ -1,9 +1,12 @@
 // Recibe el dato que envia el cliente 
+// Cuando se utiliza la función, esta recibe la información que el cliente envia, esa información pueden ser datos
+// o no, si tiene datos, escuchamos los datos y los convertimos a JSON y lo guardamos en una propiedad body
+// Cuando se quiera acceder a la información que el cliente envio lo hacemos accediendo a request.body.
 function bodyParser(request){
     
     // Para evitar usar un callback usamos las promesas
     // la promesa puede ser erronea o satisfactoria
-    new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
         let totalData = '';
         request
