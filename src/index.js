f3df80b9ff946104b53d6fab5d461e30a66c02bf
ethanial1 <camiloeiz00@gmail.com>
2021-08-19 16:28:55 -0500

    Servidor iniciar, y puerto

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ const http = require('http');
 // Creamos el servidor, en el hay una funcion que maneja las peticiones
 // request: lo que envia el usuario
 // response: lo que el servidor puede responder
-http.createServer((request, response) => {
+const server = http.createServer((request, response) => {
     // Obtenemos las partes del objeto.
     // metodos: get, post, put, delete
     const {url, method} = request;
     // imprimimos la respuesta
     console.log(`URL: ${url} - Method: ${method}`);
 
+    // Cabeceras: información del tipo de archivo
+    response.writeHead(200, {'Content-Type': 'text/plan'})
     response.write('Recibido');
-})
+    response.end();
+});
+
+// Hacemos que el servidor empiece a escuchar
+// Recibe como parámetro el puerto
+server.listen(3000);
