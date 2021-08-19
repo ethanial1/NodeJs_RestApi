3d0baee1eee524cfd39dbe60e86e041df7e330f3
ethanial1 <camiloeiz00@gmail.com>
2021-08-19 16:20:25 -0500

    creamos el servidor

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@
 
 // http: permite crear un servidor, manejar las peticiones y respuesta que llegan
 const http = require('http');
+
+// Creamos el servidor, en el hay una funcion que maneja las peticiones
+// request: lo que envia el usuario
+// response: lo que el servidor puede responder
+http.createServer((request, response) => {
+    // Obtenemos las partes del objeto.
+
+    const {url, method} = request;
+})
